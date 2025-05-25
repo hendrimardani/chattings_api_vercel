2e3f994ec8c95c2ed06c179649d1e873f827e516
hendrimardani <hendrimardani290301@gmail.com>
2025-05-26 01:23:13 +0700

    delete name umur_anak_in_month on child_service

--- supabase/migrations/20250512165716_create_child_service.sql
+++ supabase/migrations/20250512165716_create_child_service.sql
@@ -1,6 +1,5 @@
 CREATE TABLE child_service(
   id SERIAL PRIMARY KEY,
-  umur_anak_dalam_bulan VARCHAR(50) NOT NULL,
   tinggi_cm VARCHAR(3) NOT NULL,
   created_at TIMESTAMP NULL,
   updated_at TIMESTAMP NULL
