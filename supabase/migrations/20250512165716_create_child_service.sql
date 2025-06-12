3568fb0fdc060b7b32aabdd69fe654bdc380fd85
hendrimardani <hendrimardani290301@gmail.com>
2025-06-12 20:27:27 +0700

    add hasil_pemeriksaan on child_service schema

--- supabase/migrations/20250512165716_create_child_service.sql
+++ supabase/migrations/20250512165716_create_child_service.sql
@@ -1,7 +1,10 @@
+-- CREATE TYPE children_check_result AS ENUM ('NORMAL', 'STUNTING KURUS', 'STUNTING', 'STUNTING TINGGI');
+
 CREATE TABLE child_service(
   id SERIAL PRIMARY KEY,
   pemeriksaan_id INTEGER NOT NULL,
   tinggi_cm VARCHAR(3) NOT NULL,
+  hasil_pemeriksaan children_check_result NOT NULL,
   created_at TIMESTAMP NULL,
   updated_at TIMESTAMP NULL
 
